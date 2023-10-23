9b2fa89cc4a5da94bdc031a89a16f2e623b465f3
Md foisal islam~ <islammdfoisal54@gmail.com>
2023-10-23 09:55:42 -0700

    fixed some bug

--- hello.js
+++ hello.js
@@ -1,19 +1,15 @@
-let subData = [];
 const loadData = async neUrl => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${neUrl}`;
   const res = await fetch(url);
   const data = await res.json();
-  showData(data);
+  showData(data.meals.slice(0, 6));
+
+  console.log(data.meals.slice(0, 6));
 };
 
 const showData = datas => {
-  const data = datas.meals;
-  data.forEach(element => {
-    subData.push(element);
-  });
-
-  for (let i = 0; i < 6; i++) {
-    const newData = data[i];
+  for (const newData of datas) {
+    // console.log(newData);
 
     const div = document.createElement('div');
     div.classList.add('col');
@@ -43,7 +39,7 @@ document.getElementById('searchBtn').addEventListener('click', () => {
   const searchInput = document.querySelector('#searchInput').value;
   document.getElementById('cardContainer').innerHTML = '';
   loadData(`${searchInput}`);
-  console.log(searchInput);
+  // console.log(searchInput);
 });
 
 // show details
@@ -71,35 +67,17 @@ const showInnerModel = values => {
   `;
 };
 loadData('chicken');
+
 document.getElementById('showAll').addEventListener('click', () => {
-  for (const data of subData) {
-    const div = document.createElement('div');
-    div.classList.add('col');
-    div.innerHTML = `
-     <div class=" card mb-3 overflow-hidden" style="max-width: 380px; ">
-            <div class="row g-0">
-            <div class="col-md-5">
-                <img src="${data.strMealThumb}" class=" w-100 h-100 rounded-start" alt="...">
-              </div>
-              <div class="col-md-7">
-                <div class="card-body">
-                  <h5 class="card-title">${data.strMeal}</h5>
-                  <p class="card-text">There are many variations of passages of available, but the majority have suffered</p>
-                  <p class="card-text">
-                  <button onclick="showDetails(${data.idMeal})" class=" btn text-warning"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">Vew Details </button></p>
-                </div>
-              </div>
-            </div>
-          </div>
-    `;
-    if (searchInput == '') {
-      loadData('chicken');
-    } else {
-      const searchInput = document.querySelector('#searchInput').value;
-      loadData(`${searchInput}`);
-    }
-    document.getElementById('cardContainer').appendChild(div);
-  }
+  document.getElementById('cardContainer').innerHTML = '';
+  const inputvalue = document.querySelector('#searchInput').value;
+
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${
+    inputvalue === '' ? 'chicken' : inputvalue
+  }`;
+  fetch(url)
+    .then(response => response.json())
+    .then(result => showData(result.meals));
 });
 
-console.log(subData);
+// console.log(subData);
